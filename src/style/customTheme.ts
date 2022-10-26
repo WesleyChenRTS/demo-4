f26226a8382d4d71286980e0e795bdc78240c11e
IdleScV <weschen1996@gmail.com>
2022-10-26 15:18:28 -0400

    fix: remove unused import

--- src/style/customTheme.ts
+++ src/style/customTheme.ts
@@ -1,4 +1,4 @@
-import { extendTheme, StyleFunctionProps } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
 // Learn to customize theme here: https://chakra-ui.com/docs/styled-system/customize-theme
 const theme = extendTheme({});
